ebb5949ad9e9d3a54804fd095f565f4a3144188b
eun0ee <jestheroalice@gmail.com>
2026-06-01 00:29:01 +0900

    playerAnim 추가

--- CHATNYANG/Assets/Scripts/Player/PlayerController.cs
+++ CHATNYANG/Assets/Scripts/Player/PlayerController.cs
@@ -1,16 +1,19 @@
 using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(Animator))]
+[RequireComponent(typeof(SpriteRenderer))]
 public class PlayerController : MonoBehaviour
 {
     [Header("Movement")]
     [SerializeField] private float moveSpeed = 5f;
 
     private Rigidbody2D _rb;
+    private Animator _animator;
+    private SpriteRenderer _spriteRenderer;
     private Vector2 _moveInput;
     private bool _isMoving;
 
-    // 외부에서 속도 버프 적용할 수 있도록 프로퍼티로
     public float MoveSpeed
     {
         get => moveSpeed;
@@ -23,6 +26,9 @@ public class PlayerController : MonoBehaviour
     private void Awake()
     {
         _rb = GetComponent<Rigidbody2D>();
+        _animator = GetComponent<Animator>();
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+
         _rb.gravityScale = 0f;
         _rb.freezeRotation = true;
     }
@@ -30,6 +36,8 @@ public class PlayerController : MonoBehaviour
     private void Update()
     {
         GatherInput();
+        UpdateAnimation();
+        FlipSprite();
     }
 
     private void FixedUpdate()
@@ -50,5 +58,32 @@ public class PlayerController : MonoBehaviour
     private void Move()
     {
         _rb.MovePosition(_rb.position + _moveInput * (moveSpeed * Time.fixedDeltaTime));
+    }
+
+    private void UpdateAnimation()
+    {
+        // 이동 중일 때만 방향 파라미터 업데이트
+        if (_isMoving)
+        {
+            _animator.SetFloat("MoveX", _moveInput.x);
+            _animator.SetFloat("MoveY", _moveInput.y);
+        }
+
+        // 이동 속도를 전달하여 대기와 걷기 상태 전환
+        _animator.SetFloat("Speed", _moveInput.sqrMagnitude);
+    }
+
+    private void FlipSprite()
+    {
+        // 기본 애니메이션이 왼쪽 방향이므로, 오른쪽으로 이동할 때 이미지를 뒤집음
+        if (_moveInput.x > 0)
+        {
+            _spriteRenderer.flipX = true;
+        }
+        // 왼쪽으로 이동할 때는 원본 이미지 그대로 사용
+        else if (_moveInput.x < 0)
+        {
+            _spriteRenderer.flipX = false;
+        }
     }
 }
